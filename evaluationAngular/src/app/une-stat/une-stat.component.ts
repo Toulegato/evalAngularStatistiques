72e3482a4e153c593bc7dc748ee1a70cca168c6b
Bagaudin <bagaudin.sultygov@hotmail.fr>
2021-12-10 12:23:45 +0100

    Exercice 5 : Composant

---
+++ evaluationAngular/src/app/une-stat/une-stat.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Statistique } from '../models/statistique';
+
+@Component({
+  selector: 'app-une-stat',
+  templateUrl: './une-stat.component.html',
+  styleUrls: ['./une-stat.component.css']
+})
+export class UneStatComponent implements OnInit {
+  @Input() public uneStat!: Statistique;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
