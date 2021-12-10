35a32273fef545c4c3621ad0a24ac60b3cf99fd9
Bagaudin <bagaudin.sultygov@hotmail.fr>
2021-12-10 12:00:12 +0100

    Exercice 4 : Interpolation de variables

--- evaluationAngular/src/app/models/statistique.ts
+++ evaluationAngular/src/app/models/statistique.ts
@@ -1,2 +1,12 @@
 export class Statistique {
+    public identifiant: string;
+    public titre: string;
+    public valeur : string;
+
+    constructor(id: string, titre: string, valeur: string) {
+        this.identifiant = id;
+        this.titre = titre;
+        this.valeur = valeur;
+      
+    }
 }
